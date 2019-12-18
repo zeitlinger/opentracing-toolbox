d96fc0326c47f413e07ce57c46819feb833e8d1b
Willi Schönborn <w.schoenborn@gmail.com>
2019-12-18 09:19:06 +0100

    Added servlet extension auto configuration
    Fixes #341

---
+++ opentracing-servlet-extension/opentracing-servlet-extension-autoconfigure/src/main/java/org/zalando/opentracing/servlet/extension/autoconfigure/OpenTracingServletExtensionAutoConfiguration.java
@@ -0,0 +1,32 @@
+package org.zalando.opentracing.servlet.extension.autoconfigure;
+
+import io.opentracing.contrib.web.servlet.filter.ServletFilterSpanDecorator;
+import org.apiguardian.api.API;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.opentracing.servlet.extension.StandardSpanDecorator;
+
+import static org.apiguardian.api.API.Status.EXPERIMENTAL;
+
+@API(status = EXPERIMENTAL)
+@Configuration
+@ConditionalOnWebApplication
+@ConditionalOnClass(ServletFilterSpanDecorator.class)
+@AutoConfigureAfter(name =
+        "io.opentracing.contrib.spring.tracer.configuration.TracerAutoConfiguration")
+@AutoConfigureBefore(name =
+        "io.opentracing.contrib.spring.web.starter.ServerTracingAutoConfiguration")
+public class OpenTracingServletExtensionAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(ServletFilterSpanDecorator.class)
+    public ServletFilterSpanDecorator standardServletFilterSpanDecorator() {
+        return new StandardSpanDecorator();
+    }
+
+}
